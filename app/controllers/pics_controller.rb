d2f327947be1f783e9456d9d7a41e229bb3dd78c
jaykibi <raquibbi@gmail.com>
2020-09-05 03:58:27 -0500

    added devise and fixed front end, need to put links on homepage for each image

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,7 @@
 class PicsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
     def index
-    
+        @pics = Pic.last(12)
     end
 
     def new
@@ -8,8 +9,15 @@ class PicsController < ApplicationController
     end
 
     def create
-        @pic = Pic.create(pic_params)
+        @pic = current_user.pics.create(pic_params)
         redirect_to root_path
+    end
+
+    def show
+        @pic = Pic.find_by_id(params[:id])
+        if @pic.blank?
+          render plain: 'Not Found :(', status: :not_found
+        end
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "pics#index"
-  resources :pics, only: [:new, :create]
+  resources :pics, only: [:new, :create, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
